7aae2111b9378c7205854c6d0ea264bb1d2b551b
Peshani-Rasangika <140165479+Peshani-Rasangika@users.noreply.github.com>
2024-07-09 19:43:19 +0530

    FPLS4_3.scala

--- FPLS4_3.scala
+++ FPLS4_3.scala
@@ -1,11 +1,12 @@
-def toUpper(str : String) : String = str.toUpperCase()
+def toUpper(string: String): String = string.toUpperCase()
 
-def toLower(str : String) : String = str.toLowerCase()
+def toLower(string: String): String = string.toLowerCase()
 
-def formatNames(str : String)(format: String => String) = format(str)
-@main def practical4_3(): Unit = {
+def formatNames(string: String)(format: String => String) = format(string)
+
+@main def q3() = {
     println(formatNames("Benny")(toUpper))
-    println(formatNames("Niroshan")(str => toUpper(str.take(2)) + toLower(str.drop(2))))
+    println(formatNames("Niroshan")(string => toUpper(string.take(2)) + string.drop(2)))
     println(formatNames("Saman")(toLower))
-    println(formatNames("Kumara")(str => str.head + toLower(str.drop(1).init) + str.last.toUpper))
+    println(formatNames("Kumara")(string => string.head.toUpper + toLower(string.drop(1).init) + string.last.toUpper))
 }
